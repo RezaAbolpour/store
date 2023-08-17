646447c789d8471ac408b85db886f74c5bbcf69a
Reza <rezamis99vip@gmail.com>
2023-08-17 09:42:17 +0430

    create header for admin panel

---
+++ src/components/common/Header/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="bg-[#0E2954] h-16 flex items-center p-5 _font-bold text-xl" dir="rtl">
+        <p className="text-white">پنل ادمین</p>
+    </div>
+  )
+}
+
+export default Header
