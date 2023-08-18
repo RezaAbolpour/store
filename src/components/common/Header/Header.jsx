2a8c82083729d43cd0f7b1bed699c84a79f059c9
Reza <rezamis99vip@gmail.com>
2023-08-18 10:32:08 +0430

    change color

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,9 +1,21 @@
+import HomeIcon from "@mui/icons-material/Home";
+import { Link } from "react-router-dom";
 function Header() {
   return (
-    <div className="bg-[#0E2954] h-16 flex items-center p-5 _font-bold text-xl" dir="rtl">
+    <>
+      <div
+        className="bg-[#71C9CE] h-16 flex items-center p-5 _font-bold text-xl justify-between shadow-inner shadow-black"
+        dir="rtl"
+      >
         <p className="text-white">پنل ادمین</p>
-    </div>
-  )
+        <div>
+          <Link to="/">
+            <HomeIcon sx={{ fontSize: 40 }} />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
