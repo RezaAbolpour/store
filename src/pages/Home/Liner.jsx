4aeee34dcb0f03e9875ca5868e65c50cce1757d6
Reza <rezamis99vip@gmail.com>
2023-08-26 21:36:49 +0430

    line for betwin product

---
+++ src/pages/Home/Liner.jsx
@@ -0,0 +1,16 @@
+import InstagramIcon from "@mui/icons-material/Instagram";
+function Liner() {
+  return (
+    <div className="h-10 pl-20 flex items-center justify-center gap-2">
+      <span className="text-slate-400">
+        ----------------------------------------------------------------------
+      </span>
+      <InstagramIcon />
+      <span className="text-slate-400">
+        ----------------------------------------------------------------------
+      </span>
+    </div>
+  );
+}
+
+export default Liner;
