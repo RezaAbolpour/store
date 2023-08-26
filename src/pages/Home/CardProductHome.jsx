85114c78ea4b4679fe67018b3f08dafbe30abdd8
Reza <rezamis99vip@gmail.com>
2023-08-26 21:36:25 +0430

    create componnet product

---
+++ src/pages/Home/CardProductHome.jsx
@@ -0,0 +1,30 @@
+function CardProductHome() {
+  return (
+    <>
+      <div className="bg-white h-72 w-1/6 rounded-md">
+        <div className="flex items-center justify-center">
+          <img
+            src="https://hamkala.com/wp-content/uploads/Kenwood-BL770-Blender-0cd1bc.jpeg"
+            className="w-28 p-2"
+          />
+        </div>
+        <div className="flex justify-center items-center _font-bold">
+          مخلوط کن کنوود مدل BL770
+        </div>
+        <div className="h-2/6 flex items-end pl-4">
+          <bdi className="_font-bold">
+            ۷۲۷,۰۰۰&nbsp;
+            <span className="_font-fat">تومان</span>
+          </bdi>
+        </div>
+        <div className="h-1/6 flex justify-end items-end pr-4">
+          <div className="bg-yellow-500 w-10 flex justify-center items-center rounded-md text-2xl text-white">
+            +
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CardProductHome;
