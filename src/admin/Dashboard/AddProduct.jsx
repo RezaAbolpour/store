d930d17bc439372cf4a04940e9ea22099023ba40
Reza <rezamis99vip@gmail.com>
2023-08-27 17:49:49 +0430

    add border color outline

--- src/admin/Dashboard/AddProduct.jsx
+++ src/admin/Dashboard/AddProduct.jsx
@@ -2,7 +2,7 @@ import FreeSoloCreateOption from "./AutoCompletInput";
 function AddProduct() {
   return (
     <div className="h-screen flex justify-center items-center p-60">
-      <div className="bg-white w-screen h-[34rem] rounded-md">
+      <div className="bg-white w-screen h-[34rem] outline outline-offset-2 outline-2 rounded-sm outline-cyan-500">
         <div className="flex justify-center mt-4 text-lg _font-fat">
           افزودن کالا
         </div>
