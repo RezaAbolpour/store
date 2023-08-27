6cb05d1f05ece7b07aafa0ef747cab57d0bfdb9f
Reza <rezamis99vip@gmail.com>
2023-08-27 17:51:45 +0430

    add two value in initialstate

--- src/data/dataslice.js
+++ src/data/dataslice.js
@@ -1,10 +1,22 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { GetAllProduct } from "../utils/api/getProduct";
+import { GetAllCategori } from "../utils/api/getCategori";
+import { GetSubCategori } from "../utils/api/getCategori";
 const initialState = {
-  data:0
+  data: 0,
+  Categori:0,
+  subCategori:0,
 };
-export const fetchAllProduct = createAsyncThunk("user", async () => {
+export const fetchAllProduct = createAsyncThunk("product", async () => {
   return await GetAllProduct();
+});
+
+export const fetchAllCategori = createAsyncThunk("Categori", async () => {
+  return await GetAllCategori();
+});
+
+export const fetchSubCategori = createAsyncThunk("subCategori", async () => {
+  return await GetSubCategori();
 });
 
 export const dataslice = createSlice({
@@ -12,10 +24,20 @@ export const dataslice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchAllProduct.fulfilled,(state,action)=>{
-        state.data=action.payload.data.data.products
-        // console.log(state.data);
-    })
+    builder.addCase(fetchAllProduct.fulfilled, (state, action) => {
+      state.data = action.payload.data.data.products;
+      // console.log(state.data);
+    });
+
+    builder.addCase(fetchAllCategori.fulfilled, (state, action) => {
+      state.Categori = action.payload.data.data.categories;
+      // console.log(state.Categori);
+    });
+
+    builder.addCase(fetchSubCategori.fulfilled, (state, action) => {
+      state.subCategori = action.payload.data.data.subcategories;
+      console.log(state.subCategori);
+    });
   },
 });
 
