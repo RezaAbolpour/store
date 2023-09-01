a2aa420a05e7a6d95c6f230dabbae07de48d7d59
Reza <rezamis99vip@gmail.com>
2023-09-01 09:09:02 +0430

    upadte headrer site

--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -1,62 +1,34 @@
-import logo from "../../assets/Images/logoHeadrer.png";
-import ShareLocationIcon from "@mui/icons-material/ShareLocation";
-import CategoriesSelect from "./selector";
+import PhoneEnabledIcon from "@mui/icons-material/PhoneEnabled";
 import SearchIcon from "@mui/icons-material/Search";
-import AccountMenue from "./AccountList";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import KeyIcon from "@mui/icons-material/Key";
 function Header() {
   return (
-    <div className="bg-slate-950 h-28 pr-3 pl-3 flex _font-bold max-sm:flex-col max-sm:h-44" dir="rtl">
-      <div className="w-1/6 flex items-center h-full max-sm:hidden">
-        <img src={logo} alt="" className="h-16 mr-5" />
-      </div>
-      <div className="flex justify-center flex-col w-1/6 gap-2 max-sm:hidden">
-        <div>
-          <p className="text-white">آدرس تحویل</p>
+    <div className="h-[174px] bg-red-500">
+      <div className="h-[55px] bg-[#EDEDED]"></div>
+      <div className="h-[74px] bg-white pr-5 flex gap-10" dir="rtl">
+        <div className="w-[83px] h-full flex items-center justify-center">
+          <img src="https://hamkala.com/wp-content/uploads/lg3.png" alt="" />
         </div>
-        <div className="flex gap-2">
-          <ShareLocationIcon color="primary" />
-          <p className="text-white">Holand</p>
-        </div>
-      </div>
-      <div className="flex items-center w-2/6">
-        <div className="flex justify-center bg-white rounded-md p-2" dir="ltr">
-          <div>
-            <CategoriesSelect />
-          </div>
-          <div className="border flex items-center">
+        <div className="w-3/6  flex items-center pr-[20px] pl-[20px]">
+          <div className=" w-full h-[50px] rounded-md flex items-center border-solid border-2 border-gray-200 shadow-2xl">
+            <SearchIcon />
             <input
               type="text"
-              placeholder="جستجو در آمازون"
-              dir="rtl"
-              className="outline-none pr-2"
+              placeholder="جستجو برای محصول..."
+              className="flex items-center h-full w-[80%] outline-none mr-5"
             />
           </div>
-          <div className="bg-yellow-200 flex items-center p-4">
-            <SearchIcon />
+        </div>
+        <div className="w-2/6 flex items-center justify-end">
+          <div className="h-[50px] w-[150px] border-solid border-2 border-gray-200 shadow-2xl rounded-md flex items-center justify-center gap-3">
+            <KeyIcon />
+            <p className="_font-bold">ورود / ثبت نام </p>
           </div>
         </div>
       </div>
-      <div className="w-2/6 flex justify-end items-center  max-sm:w-full max-sm:justify-between">
-        <div className=" flex p-2 flex-col justify-center max-sm:w-3/6">
-          <div className="text-white">سلام وارد حساب بشوید</div>
-          <div>
-            <AccountMenue />
-          </div>
-        </div>
-        <div className="max-sm:hidden">
-          <ShoppingCartIcon color="primary" sx={{ fontSize: 70 }} />
-        </div>
-        
-        <div className="flex justify-center flex-col max-sm:w-3/6 gap-2 sm:hidden  max-sm:items-end">
-        <div>
-          <p className="text-white">آدرس تحویل</p>
-        </div>
-        <div className="flex gap-2 max-sm:dire">
-          <ShareLocationIcon color="primary" />
-          <p className="text-white">Holand</p>
-        </div>
-      </div>
+      <div className="h-[45px] bg-[#EDEDED] flex items-center justify-end gap-5 pr-5">
+        <p className="_font-medium">تماس با ما</p>
+        <PhoneEnabledIcon />
       </div>
     </div>
   );
