09f0eaa514105789003b8f0f02ac4c646377ab7e
Reza <rezamis99vip@gmail.com>
2023-09-01 09:09:41 +0430

    componnet propsal

---
+++ src/pages/Home/ProposalProduct.jsx
@@ -0,0 +1,18 @@
+function ProposalProduct() {
+  return (
+    <div className="w-1/6">
+      <div className="pl-14">
+        <img
+          src="https://hamkala.com/wp-content/uploads/برقی-دسینی-مدل-ST-601S-1-e1568115981702.jpg"
+          alt=""
+          className="w-[125px] h-[125px]"
+        />
+      </div>
+      <p className="text-center text-sm _font-medium mt-1">
+        آسیاب و خردکن و مخلوط کن
+      </p>
+    </div>
+  );
+}
+
+export default ProposalProduct;
