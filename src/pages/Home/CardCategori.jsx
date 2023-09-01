6895705a8590889de308bb2f5f4aec39a0443e40
Reza <rezamis99vip@gmail.com>
2023-09-01 09:07:59 +0430

    create card categori

--- src/pages/Home/CardCategori.jsx
+++ src/pages/Home/CardCategori.jsx
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
+
 function CardCategori(propse) {
+  let id = propse.idCategor;
+  let link =`/product-categori/${id}`
   return (
-    <div className="h-32 w-40 rounded-md bg-gray-200">
-      <div className="flex justify-center items-center">
-        <img src={propse.img} alt="" />
+    <Link to={link}>
+      <div className="h-32 w-40 rounded-md bg-gray-200">
+        <div className="flex justify-center items-center">
+          <img src={propse.img} alt="" />
+        </div>
+        <div className="flex justify-center items-center mt-3">
+          <p className="_font-bold text-sm">{propse.name}</p>
+        </div>
       </div>
-      <div className="flex justify-center items-center mt-3">
-        <p className="_font-bold text-sm">{propse.name}</p>
-      </div>
-    </div>
+    </Link>
   );
 }
 
