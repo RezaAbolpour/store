4de4150ec513a0ae3dd24f82e6ddac7f2dcf36a4
Reza <rezamis99vip@gmail.com>
2023-09-01 09:05:50 +0430

    add new route

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import Login from "./pages/Login/Login";
 import AdminLogin from './admin/Login/Login'
 import NoPage from "./pages/ErrorPage/NoPage";
 import Dashboard from "./admin/Dashboard/Dashboard";
+import ProductPage from "./pages/ProductPage/ProductPage";
 import { Navigate } from "react-router-dom";
+import Product from "./pages/ProductPage/Product";
+import ProductPageSub from "./pages/ProductPage/ProducPageCategori";
 function App() {
   return (
     <div className="App">
@@ -17,6 +20,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Navigate replace to="/" />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/product-categori/*" element={<ProductPage/>}/>
+          <Route path="/product-subCategori/*" element={<ProductPageSub/>}/>
+          <Route path="/product/*" element={<Product/>}/>
           <Route path="*" element={<NoPage/>} />
         </Routes>
       </BrowserRouter>
