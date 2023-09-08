52ba98d5f242be049b13066cee1e3c1ba535748e
Reza <rezamis99vip@gmail.com>
2023-09-08 14:55:16 +0430

    connet api

--- src/pages/Home/ProposalProduct.jsx
+++ src/pages/Home/ProposalProduct.jsx
@@ -1,16 +1,19 @@
-function ProposalProduct() {
+import { Link } from "react-router-dom";
+
+function ProposalProduct({ data }) {
+  let link = `/product/${data._id}`;
   return (
     <div className="w-1/6">
-      <div className="pl-14">
-        <img
-          src="https://hamkala.com/wp-content/uploads/برقی-دسینی-مدل-ST-601S-1-e1568115981702.jpg"
-          alt=""
-          className="w-[125px] h-[125px]"
-        />
-      </div>
-      <p className="text-center text-sm _font-medium mt-1">
-        آسیاب و خردکن و مخلوط کن
-      </p>
+      <Link to={link}>
+        <div className="pl-14">
+          <img
+            src={`http://localhost:8000/images/products/images/${data.images[0]}`}
+            alt=""
+            className="w-[125px] h-[125px]"
+          />
+        </div>
+        <p className="text-center text-sm _font-medium mt-1">{data.name}</p>
+      </Link>
     </div>
   );
 }
