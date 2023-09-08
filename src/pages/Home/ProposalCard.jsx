6add8f96a03d97712cb6812bfc5ce2df5165a761
Reza <rezamis99vip@gmail.com>
2023-09-08 14:51:16 +0430

    edit cart for connet api

--- src/pages/Home/ProposalCard.jsx
+++ src/pages/Home/ProposalCard.jsx
@@ -1,25 +1,48 @@
-function ProposalCard() {
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { setorder } from "../../data/dataslice";
+import ModalMessageHome from "./ModalMessageHome";
+
+function ProposalCard({ data }) {
+  let link = `/product/${data._id}`;
+  const [showModal, setShowModal] = useState(false);
+  const dispach = useDispatch();
+  function addbox(idProduct) {
+    setShowModal(true);
+    dispach(setorder({id:idProduct,count:1}));
+  }
+  function closeModal() {
+    setShowModal(false);
+  }
   return (
     <div className="w-1/6">
-      <div className="pl-10 pt-5">
-        <img
-          src="https://hamkala.com/wp-content/uploads/Blender-Kenwood-SB32732c3cd.jpeg"
-          alt=""
-          className="w-[150px] h-[150px]"
-        />
-        <p className="text-sm _font-medium mt-1">مخلوط کن کنوود مدل SB327</p>
-      </div>
+      <Link to={link}>
+        <div className="pl-10 pt-5">
+          <img
+            src={`http://localhost:8000/images/products/images/${data.images[0]}`}
+            alt=""
+            className="w-[150px] h-[150px]"
+          />
+          <p className="text-sm _font-medium mt-1">{data.name}</p>
+        </div>
+      </Link>
       <div className="flex justify-between pl-3 pr-3 items-center">
         <bdi className="_font-bold">
-          ۷۲۷,۰۰۰&nbsp;
+          {data.price}&nbsp;
           <span className="_font-fat">تومان</span>
         </bdi>
+
         <div>
-          <div className="bg-yellow-500 w-10 flex justify-center items-center rounded-md text-2xl text-white">
+          <div
+            className="bg-yellow-500 w-10 flex justify-center items-center rounded-md text-2xl text-white"
+            onClick={() => addbox(data._id)}
+          >
             +
           </div>
         </div>
       </div>
+      {showModal && <ModalMessageHome onCloce={closeModal} />}
     </div>
   );
 }
