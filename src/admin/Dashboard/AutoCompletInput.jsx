ead47771b93a8cceebe499bc8c4e25151710eaca
Reza <rezamis99vip@gmail.com>
2023-09-08 14:41:04 +0430

    edit code add product

--- src/admin/Dashboard/AutoCompletInput.jsx
+++ src/admin/Dashboard/AutoCompletInput.jsx
@@ -22,8 +22,13 @@ const keyAddProduct = [
   "thumbnail",
 ];
 
+function createdImge(file) {
+  let fil=file[0]
+  let bold = new Blob([fil], { type: fil.type });
+  return URL.createObjectURL(bold);
+}
 export default function FreeSoloCreateOption() {
-  const[showmessage,setshowmessage]=React.useState(0)
+  const [showmessage, setshowmessage] = React.useState(0);
   const [valueCategori, setValueCategori] = React.useState(null);
   const [valueSubCategori, setValueSubCategori] = React.useState(null);
   const [valueNameProduct, setValueNameProduct] = React.useState(null);
@@ -33,6 +38,7 @@ export default function FreeSoloCreateOption() {
   const [valueDiscription, setValueDiscription] = React.useState(null);
   const [selectedFile, setSelectedFile] = React.useState("");
   const [selectedFilePictur, setSelectedFilePictur] = React.useState(null);
+  const [SelectedFilePicturShow,setSelectedFilePicturShow] = React.useState(null);
   console.log(valueCategori, valueSubCategori);
   console.log(selectedFilePictur);
   if (!valueCategori) {
@@ -93,6 +99,8 @@ export default function FreeSoloCreateOption() {
     // for (let key in event.target.files) {
     //   file.push(event.target.files[key]);
     // }
+    setSelectedFilePicturShow(event.target.value);
+    console.log(SelectedFilePicturShow);
     setSelectedFilePictur(event.target.files);
     console.log(selectedFilePictur);
   }
@@ -146,9 +154,9 @@ export default function FreeSoloCreateOption() {
       setValuePrice("");
       setSelectedFilePictur(null);
       setSelectedFilePictur(null);
-      setshowmessage(1)
+      setshowmessage(1);
     } else {
-      alert("no");
+      alert("تمام فیلد هارا پر کنید");
     }
   }
   function handeldelet() {
@@ -320,10 +328,7 @@ export default function FreeSoloCreateOption() {
       </div>
 
       <div className="flex justify-around pl-[48px] pr-[48px] gap-[30px]">
-        <div
-          className="flex items-center border-solid border-2 border-sky-300 h-[50px] rounded-md"
-          dir="rtl"
-        >
+        <div className="flex items-center h-[50px]" dir="rtl">
           <span className="text-sm w-2/6 text-center">انتخاب تصویر</span>
           <div className="w-4/6">
             <input
@@ -335,10 +340,7 @@ export default function FreeSoloCreateOption() {
             />
           </div>
         </div>
-        <div
-          className="flex items-center border-solid border-2 border-sky-300 ml-[60px] h-[50px] rounded-md"
-          dir="rtl"
-        >
+        <div className="flex items-center   ml-[60px] h-[50px]" dir="rtl">
           <span className="text-sm w-2/6 text-center">پیش نمایش</span>
           <div className="w-4/6">
             <input
@@ -360,6 +362,7 @@ export default function FreeSoloCreateOption() {
         </div>
         <div className="h-full w-full ml-2">
           <textarea
+            dir="rtl"
             value={valueDiscription}
             onChange={handlediscription}
             name=""
@@ -370,7 +373,73 @@ export default function FreeSoloCreateOption() {
           ></textarea>
         </div>
       </div>
-      {showmessage?(<CustomizedSnackbars open={true} type={"success"} message={"با موفقیت اضافه شد"} fun={setshowmessage} />):console.log("opps")}
+      {showmessage ? (
+        <CustomizedSnackbars
+          open={true}
+          type={"success"}
+          message={"با موفقیت اضافه شد"}
+          fun={setshowmessage}
+        />
+      ) : (
+        console.log("opps")
+      )}
+      <div
+        id="large-modal"
+        tabIndex="-1"
+        className="fixed top-0 left-0 right-0 z-50 hidden  w-full  p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      >
+        <div className="relative w-full max-w-4xl max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                نمایش عکس ها
+              </h3>
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                data-modal-hide="large-modal"
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+            </div>
+            <div className="p-6 space-y-6">
+              {/* {console.log([SelectedFilePicturShow])}
+              {<img src={SelectedFilePicturShow[0]} alt="" />} */}
+            </div>
+            <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+              <button
+                data-modal-hide="large-modal"
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                I accept
+              </button>
+              <button
+                data-modal-hide="large-modal"
+                type="button"
+                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+              >
+                Decline
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
