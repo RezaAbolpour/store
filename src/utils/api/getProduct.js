3ad8eccc3bdab78aaaa77e4648c7b4506646f8cf
Reza <rezamis99vip@gmail.com>
2023-09-08 14:56:54 +0430

    add two class for get data limit

--- src/utils/api/getProduct.js
+++ src/utils/api/getProduct.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 import { BASE_URL } from "../../configs/urlBase.js";
+import publicAxios from "../instances/publicAxios.js";
 
 export function GetAllProduct(){
     return axios.get(`${BASE_URL}/products`)
+}
+export const getProductLimit = (_page, _limit) => {
+    return publicAxios.get(`${BASE_URL}/products?page=${_page}&limit=${_limit}`)
+}
+
+export function getProductSubCategory(subCategory,limit=10,page=1){
+    return publicAxios.get(`${BASE_URL}/products?subcategory=${subCategory}&page=${page}&limit=${limit}`)
 }
