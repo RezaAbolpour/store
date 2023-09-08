905a676e58a45f11f63b93ec318872c3b3f36438
Reza <rezamis99vip@gmail.com>
2023-09-08 14:54:03 +0430

    add new class for style

--- src/pages/ProductPage/ProductCategori.jsx
+++ src/pages/ProductPage/ProductCategori.jsx
@@ -6,9 +6,9 @@ function ProductCategori(propse) {
   let id = propse.dataProduct._id;
   let link = `/product-subCategori/${id}`;
   return (
-    <div className="w-1/6 p-1 border-l-2 border-b-2 border-slate-300 h-[150px] flex justify-center items-center">
+    <div className="w-1/6 p-1 border-l-2 border-2 border-slate-300 h-[80px] flex justify-center items-center rounded-md hover:bg-slate-400 hover:h-[95px] hover:shadow-2xl hover:text-white">
       <Link to={link}>
-        <p className="text-2xl font-medium mt-1">{propse.dataProduct.name}</p>
+        <p className="text-sm _font-bold mt-1">{propse.dataProduct.name}</p>
       </Link>
     </div>
   );
